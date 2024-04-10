96d0f3229b082ea8a609296d1a204eb8f567a38d
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-04-10 17:14:49 -0300

    issues with FabricaConnection

---
+++ Models/CriadorConexao.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace ProjetoPixelPlace.Models
+{
+    public class CriadorConexao
+    {
+        public CriadorConexao()
+        {
+        }
+
+        public static MySqlConnection getConexao()
+        {
+            
+            MySqlConnection conexao = new MySqlConnection(Configuration().GetConnectionString("casa"));
+
+            if (conexao == null) {
+                return new MySqlConnection(
+                Configuration().GetConnectionString("ConexaoPadrao"));
+            }
+            return conexao;
+        }
+
+        private static IConfigurationRoot Configuration()
+        {
+            IConfigurationBuilder builder =
+                new ConfigurationBuilder().SetBasePath(
+                    Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true);
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoPixelPlace.Entities;
+using System.Collections.Generic;
+
+namespace ProjetoPixelPlace.Models
+{
+    public class UsuarioModel
+    {
+        public UsuarioModel()
+        {
+          
+        }
+
+        public List<Usuario> getAllUser()
+        {
+            MySqlConnection con = CriadorConexao.getConexao();
+            List < Usuario > users = new List<Usuario>();
+            Usuario user = new Usuario(1, "2", "3", "4", "23");
+            users.Add(user);
+
+            return users;
+
+            
+
+            /*
+                MySqlCommand comando = new MySqlCommand("Select * from usuario", con);
+                MySqlDataReader reader = comando.ExecuteReader();
+                while(reader.Read())
+                {
+                    Usuario usuario = new Usuario(int.Parse(reader["idUsuario"].ToString()),
+                        reader["NomeUser"].ToString(),
+                        reader["UrlImage"].ToString(),
+                        reader["Email"].ToString(),
+                        reader["Senha"].ToString());
+
+                    users.Add(usuario);
+                }
+                con.Close();
+            }
+            */
+            
+                
+               
+            
+            
+        }
+    }
+}
