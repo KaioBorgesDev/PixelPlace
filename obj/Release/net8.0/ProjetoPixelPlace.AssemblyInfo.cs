dbfba16cf520cd0bb9c8c68e9850b4a1fef4c928
KaioBorgesDev <kaioeduardo401@outlook.com>
2024-05-02 16:51:37 -0300

    Comprar Jogo - incompleted

--- obj/Release/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
+++ obj/Release/net8.0/ProjetoPixelPlace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b6c442aa943b19cb2314ed12e931a6dd6128ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6d10b2c0cd3beca7ca15d77265e0ee77c33211")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoPixelPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
